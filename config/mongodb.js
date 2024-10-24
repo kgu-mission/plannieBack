a238254d80e10a907735cf737f63419de05e084b
seojin <juytj21@gmail.com>
2024-10-24 22:01:08 +0900

    mongodb

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        // 환경 변수에서 MongoDB URI를 가져옴 (.env에서 MONGO_URI 사용)
+        const mongoURI = process.env.MONGO_URI;
+        if (!mongoURI) {
+            throw new Error('MongoDB 연결 URL이 설정되지 않았습니다.');
+        }
+
+        // MongoDB에 연결
+        await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected');
+        console.log('MongoDB 연결 성공');
     } catch (err) {
         console.error('MongoDB connection error:', err);
-        process.exit(1);
+        process.exit(1);  // MongoDB 연결 실패 시 애플리케이션 종료
     }
 };
 
