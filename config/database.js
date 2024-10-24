b122eb65b3990d66b78ae3e08db58735e21743c6
seojin <juytj21@gmail.com>
2024-10-24 21:18:04 +0900

    rds

--- config/database.js
+++ config/database.js
@@ -5,13 +5,13 @@ const sequelize = new Sequelize('planie', 'admin', 'qwer1234', {
     host: 'database-planie.cr4w208kyjvg.ap-northeast-2.rds.amazonaws.com',  // RDS 엔드포인트
     dialect: 'mariadb',
     dialectOptions: {
-        timezone: "Etc/GMT0"  // 타임존 설정
+        timezone: '+00:00'  // 타임존 설정
     },
     logging: false,  // 로깅 비활성화
     pool: {
         max: 5,  // 최대 연결 수
         min: 0,  // 최소 연결 수
-        acquire: 30000,  // 연결 시도 최대 시간 (밀리초)
+        acquire: 60000,  // 연결 시도 최대 시간 (밀리초)
         idle: 10000  // 연결이 사용되지 않고 유지되는 최대 시간
     }
 });
